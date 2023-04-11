a98d8bc99f4eeee10e293c06d8ba45472552d085
Branlyn <branlyn777@hotmail.com>
2023-04-11 16:01:56 -0400

    Ruta Inicio por defecto

--- routes/web.php
+++ routes/web.php
@@ -25,6 +25,10 @@ Route::get('/dashboard', function () {
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
+
+    //Ruta por defecto despues de iniciar sesión :)
+    Route::redirect('/', 'inicio');
+
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
