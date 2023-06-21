d84cdfbebde6e22d2c90c29860074f8b803e4b88
Branlyn <branlyn.mamani@assuresoftbolivia.com>
2023-06-21 09:45:24 -0400

    Nuevo template landing

--- resources/views/livewire/landing/catalogue.blade.php
+++ resources/views/livewire/landing/catalogue.blade.php
@@ -7,31 +7,51 @@
         }
     </style>
 @endsection
-<div>
 
-
-    <div class="container px-2 my-2">
-        <div class="text-center mb-5">
-            <h1 class="display-5 fw-bolder mb-0"><span class="text-gradient d-inline">Catálogo Productos</span></h1>
-        </div>
-        <div class="row justify-content-center">
-
-
-          @foreach($products as $p)
-          <div class="col-12 col-sm-6 col-md-3">
-              <div class="card">
-                  <img src="{{ asset('storage/invProducts/' . $p->image) }}" class="card-img-top" alt="...">
-                  <div class="card-body">
-                      <h5 class="card-title">{{ $p->name_product }}</h5>
-                      <p class="card-text">{{ $p->description_catalogue }}</p>
-                      <a href="#" class="btn btn-primary">Ver Detalles</a>
-                  </div>
-              </div>
-          </div>
-          @endforeach
-
-
-        </div>
+<div class="row">
+    <div class="product-active owl-carousel">
+        @foreach($products as $p)
+            <div class="col-lg-12">
+                <!-- single-product-wrap start -->
+                <div class="single-product-wrap">
+                    <div class="product-image">
+                        <a href="landing/single-product.html">
+                            <img src="{{ asset('storage/invProducts/' . $p->image) }}" alt="Li's Product Image">
+                        </a>
+                        <span class="sticker">Nuevo</span>
+                    </div>
+                    <div class="product_desc">
+                        <div class="product_desc_info">
+                            <div class="product-review">
+                                <h5 class="manufacturer">
+                                    <a href="landing/shop-left-sidebar.html">Graphic Corner</a>
+                                </h5>
+                                <div class="rating-box">
+                                    <ul class="rating">
+                                        <li><i class="fa fa-star-o"></i></li>
+                                        <li><i class="fa fa-star-o"></i></li>
+                                        <li><i class="fa fa-star-o"></i></li>
+                                        <li class="no-star"><i class="fa fa-star-o"></i></li>
+                                        <li class="no-star"><i class="fa fa-star-o"></i></li>
+                                    </ul>
+                                </div>
+                            </div>
+                            <h4><a class="product_name" href="landing/single-product.html">{{ $p->name_product }}</a></h4>
+                            <div class="price-box">
+                                <span class="new-price">50,00 Bs</span>
+                            </div>
+                        </div>
+                        <div class="add-actions">
+                            <ul class="add-actions-link">
+                                <li class="add-cart active"><a href="landing/#">Añadir Carrito</a></li>
+                                <li><a class="links-details" href="landing/wishlist.html"><i class="fa fa-heart-o"></i></a></li>
+                                <li><a href="landing/#" title="quick view" class="quick-view-btn" data-toggle="modal" data-target="#exampleModalCenter"><i class="fa fa-eye"></i></a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <!-- single-product-wrap end -->
+            </div>
+        @endforeach
     </div>
-
 </div>
