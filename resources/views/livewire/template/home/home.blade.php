0afeb5491255af315b593fe9cf4be22dc43ae79d
Branlyn <branlyn777@gmail.com>
2024-08-31 14:07:45 -0400

    Fix

--- resources/views/livewire/template/home/home.blade.php
+++ resources/views/livewire/template/home/home.blade.php
@@ -1,39 +1,7 @@
 <div class="pc-content">
   <div class="row">
     <!-- [ sample-page ] start -->
-    <div class="col-xl-4 col-md-6">
-      <div class="card bg-primary-dark dashnum-card text-white overflow-hidden">
-        <span class="round small"></span>
-        <span class="round big"></span>
-        <div class="card-body">
-          <div class="row">
-            <div class="col">
-              <div class="avtar avtar-lg">
-                <i class="text-white ti ti-credit-card"></i>
-              </div>
-            </div>
-            <div class="col-auto">
-              <div class="btn-group">
-                <a
-                  type="button"
-                  class="avtar bg-primary dropdown-toggle arrow-none"
-                  data-bs-toggle="dropdown"
-                  aria-expanded="false"
-                >
-                  <i class="ti ti-dots"></i>
-                </a>
-                <ul class="dropdown-menu dropdown-menu-end">
-                  <li><button class="dropdown-item" type="button">Import Card</button></li>
-                  <li><button class="dropdown-item" type="button">Export</button></li>
-                </ul>
-              </div>
-            </div>
-          </div>
-          <span class="text-white d-block f-34 f-w-500 my-2">1350 <i class="ti ti-arrow-up-right-circle opacity-50"></i></span>
-          <p class="mb-0 opacity-50">Total Ventas</p>
-        </div>
-      </div>
-    </div>
+    
     <!-- [ sample-page ] end -->
 </div>
 </div>
